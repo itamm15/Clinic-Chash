f719a138bfc459fb3430ee416368440737e52650
itamm15 <osinski.mateusz15@gmail.com>
2025-04-10 22:57:34 +0200

    dump cards into partial view/component

--- Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
+++ Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc6acd75d7ee938b16cf2d172bf91e34126748c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6802fee1b79becd280822ff4f1d526d005b7e0ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
