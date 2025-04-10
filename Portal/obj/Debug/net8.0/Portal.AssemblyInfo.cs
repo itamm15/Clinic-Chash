5dd1841e7d84a88d74f6dcd2188cc92e8bc10452
itamm15 <osinski.mateusz15@gmail.com>
2025-04-10 21:58:16 +0200

    Portal Layout

--- Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
+++ Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f62a09a565feca07d3b944136957845d4253df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ca6dea94cf7aa5de8dd4b2bab53b09db7b603b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
