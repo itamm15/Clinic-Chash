c7c568d024b62540498ecb869f8f86091feaaf70
itamm15 <osinski.mateusz15@gmail.com>
2025-04-13 13:23:31 +0200

    add basic services page

--- Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
+++ Portal/obj/Debug/net8.0/Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50b8531793300c4c06769b1f46e8e80247939ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c9f111462da1885b21ec0266aaddde91ce40e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
