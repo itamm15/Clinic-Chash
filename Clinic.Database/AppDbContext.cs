4fcccbb914d26fb4d39cbac418ac48f09fe9ec55
itamm15 <osinski.mateusz15@gmail.com>
2025-05-09 21:29:58 +0200

    chore: add patients model

---
+++ Clinic.Database/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Clinic.Database
+{
+  public class AppDbContext : DbContext
+  {
+
+    public DbSet<Patient> Patients { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+  }
+}
