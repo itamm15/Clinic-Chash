282ebf1359f31f5164304895ae8d81f70c3e14b9
itamm15 <osinski.mateusz15@gmail.com>
2025-05-11 18:49:18 +0200

    chore: prettier doctors page

--- Clinic.Database/obj/Debug/net8.0/Clinic.Database.AssemblyInfo.cs
+++ Clinic.Database/obj/Debug/net8.0/Clinic.Database.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clinic.Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440abbe15cf4d329c429a5922776462ee19f7b1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2807a989a35e367b454be307c4addae122e43842")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clinic.Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clinic.Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
